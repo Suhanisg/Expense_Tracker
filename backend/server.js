29099a1db8d3d3976c2107ce6267b722f218f561
Suhanisg <suhugoyal@gmail.com>
2025-07-20 18:15:33 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,3 @@ app.use("/uploads",express.static(path.join(__dirname,"uploads")));
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
-const __dirname=path.resolve();
