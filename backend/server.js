a1c6170727eec4f1e12bc071570b1a700acf1d34
Suhanisg <suhugoyal@gmail.com>
2025-07-23 23:17:37 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,47 @@
 require("dotenv").config();
-const express=require("express");
-const cors=require("cors");
-const path=require("path");
-const connectDB=require("./config/db.js");
-const authRoutes=require("./routes/authRoutes");
-const incomeRoutes = require('./routes/IncomeRoutes');
-const expenseRoutes=require("./routes/expenseRoutes");
-const dashboardRoutes=require("./routes/dashboardRoutes");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db.js");
 
-const app=express();
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/IncomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+
+const app = express();
 
 app.use(
     cors({
-        origin:process.env.CLIENT_URL || "*",
-        methods:["GET","POST","PUT","DELETE"],
-        allowedHeaders:["Content-Type","Authorization"],
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
 app.use(express.json());
 
+// Connect DB
 connectDB();
 
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/income",incomeRoutes);
-app.use("/api/v1/expense",expenseRoutes);
-app.use("/api/v1/dashboard",dashboardRoutes);
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"frontend/build")));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"frontend","build","index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
 }
 
-//server uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
